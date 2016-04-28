d17442ab8523d20bb48d91899dfb97449d392c96
sw00 <sett.wai@gmail.com>
2016-04-28 17:11:03 +0200

    create a proper bootstrap script like a proper dev

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,32 @@
-#backup old .vim directory
-mv $HOME/.vim $HOME/.vim.old
-mv $HOME/.vimrc $HOME/.vimrc.old
+#!/bin/bash
+set -e
 
-# create symlinks for repo
-ln -s $PWD/vim $HOME/.vim
-ln -s $PWD/vimrc $HOME/.vimrc
+BASE_DIR="$(dirname ${BASH_SOURCE})"
+cd $BASE_DIR;
+
+git pull origin master;
+
+function vim() {
+    echo "Bootstrapping vim config"
+    git submodule update
+    rsync --exclude ".git/" -avh --no-perms "${BASE_DIR}/vim/" ~/.vim
+}
+
+function zsh(){
+    echo "Bootstrapping zsh config"
+    ln -sf "${BASE_DIR}/zshrc" ~/.zshrc
+    [[ "$OSTYPE" = "darwin"* ]] && ln -sf "${BASE_DIR}/zshrc.osx" ~/.zshrc.local
+}
+
+function tmux(){
+    echo "Bootstrapping tmux config"
+    ln -sf "${BASE_DIR}/tmux.conf" ~/.tmux.conf
+}
+
+INIT_LIST=(vim zsh tmux)
+
+for func in ${INIT_LIST[@]}
+do
+    $func
+    unset $func
+done
