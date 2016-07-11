679a8c52925a5c486d3f97f5501416591234e816
sw00 <sett.wai@gmail.com>
2016-07-11 22:11:54 +0300

    enable unstaged changes in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,8 +2,10 @@ autoload -Uz vcs_info
 setopt PROMPT_SUBST
 
 zstyle ':vcs_info:git:*' stagedstr '%B%F{yellow}+%f%b'
-zstyle ':vcs_info:git:*' formats '[%b%c]'
+zstyle ':vcs_info:git:*' unstagedstr '%B%F{red}*%f%b'
+zstyle ':vcs_info:git:*' formats '[%b%u%c]'
 zstyle ':vcs_info:git:*' check-for-staged-changes true
+zstyle ':vcs_info:git:*' check-for-changes true
 
 job="%1(j.%B%F{red}*%f%b.)"
 PROMPT="%B%(?..[%?] )%b%n@%U%m%u$job> "
