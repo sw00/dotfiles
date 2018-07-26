46873a220a288db4d2c887c274a75de8c5731862
sw00 <exec@sett.sh>
2018-07-26 08:37:34 +0200

    Install jump

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,6 +8,9 @@ set -U EDITOR nvim
 # TimeWarrior
 set -x TIMEWARRIORDB "$HOME/Dropbox/etc/timewarrior"
 
+# Jump
+status --is-interactive; and . (jump shell | psub)
+
 # pyenv-virtualenv
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
