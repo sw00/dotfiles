1963700fa43ff0278ef2566c6e8cc4fed70f6cad
Sett Wai <exec@sett.sh>
2018-09-24 08:16:45 +0200

    Add vscode config

---
+++ vscode/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+code --install-extension vscodevim.vim
+code --install-extension esbnp.prettier-vscode
+code --install-extension sbrink.elm
+code --install-extension gamunu.vscode-yarn
