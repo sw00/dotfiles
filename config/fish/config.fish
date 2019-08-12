2bcb1438c4493030d8982e3be9aa960cfe497d77
Sett Wai <exec@sett.sh>
2019-08-12 10:07:31 +0200

    Move aliases to fish_user_aliases.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,19 +1,6 @@
 # TimeWarrior
 set -x TIMEWARRIORDB "$HOME/Dropbox/etc/timewarrior"
 
-# Aliases
-alias abacaxi='brew update ; and brew doctor ; and brew outdated'
 
-alias vi=nvim
-alias vim=nvim
-
-alias gco='git checkout'
-alias gst='git status'
-alias gc='git commit'
-alias ga='git add'
-alias gl='git log'
-alias gd='git diff'
-
-alias doco='docker-compose'
-alias doma='docker-machine'
-
+# cabal
+set -g fish_user_paths ~/.cabal/bin $fish_user_paths
---
+++ config/fish/functions/fish_user_aliases.fish
@@ -0,0 +1,22 @@
+if [ (command -sq nvim) ]
+	alias vi='nvim'
+	alias vim='nvim'
+end
+
+if [ (uname -s) = 'Darwin' ]
+	alias opn='open'
+	alias abacaxi='brew update ; and brew doctor ; and brew outdated'
+else
+	alias opn='xdg-open'
+	(grep -q Microsoft /proc/version); and alias opn='wslview'
+end
+
+alias gco='git checkout'
+alias gst='git status'
+alias gc='git commit'
+alias ga='git add'
+alias gl='git log'
+alias gd='git diff'
+
+alias doco='docker-compose'
+alias doma='docker-machine'
