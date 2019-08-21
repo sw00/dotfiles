567de51f933ac58e4f268839f30fd2a5f75e3740
Sett Wai <exec@sett.sh>
2019-08-21 11:59:26 +0200

    Add autojump and omf git aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,8 @@
 # TimeWarrior
 set -x TIMEWARRIORDB "$HOME/Dropbox/etc/timewarrior"
 
-
 # cabal
 set -g fish_user_paths ~/.cabal/bin $fish_user_paths
+
+# autojump
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
