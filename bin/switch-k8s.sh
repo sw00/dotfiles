c4b16e8e98a0f65018a61780ba0385486020dde0
Sett Wai <exec@sett.sh>
2020-02-21 08:49:15 +0200

    Delete in-place kubeconfig

--- bin/switch-k8s.sh
+++ bin/switch-k8s.sh
@@ -4,14 +4,17 @@ case "$1"
 in
 	ska)
 		echo switching to engageska
+		rm -f ~/.kube/config
 		cp -f ~/.kube/.engageska.kubeconfig ~/.kube/config
 	;;
 	ska2)
 		echo switching to ct-test
+		rm -f ~/.kube/config
 		cp -f ~/.kube/.ct-test.kubeconfig ~/.kube/config
 	;;
 	local)
 		echo switching to local
+		rm -f ~/.kube/config
 		cp -f ~/.kube/.local.kubeconfig ~/.kube/config
 	;;
 esac
