7327e45ebf556bb5f8a6bff7cdb201b911df76a9
Sett Wai <exec@sett.sh>
2020-07-01 01:36:05 +0200

    Move exports to bash_imports

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,4 @@
 if grep -qEi '(microsoft|wsl)' /proc/version
-	set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-	set -x LIBGL_ALWAYS_INDIRECT 1
-	set -x VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1
-
 	fenv "eval `dbus-launch`" > /dev/null
 	fenv "eval `gnome-keyring-daemon -r -d -c secrets,ssh,pkcs11`" > /dev/null
 end
--- tag-wsl/bash_imports/base.sh
+++ tag-wsl/bash_imports/base.sh
@@ -1,7 +1,10 @@
-  # use VcXsrv on WSL to share clipboard
-  LOCAL_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
-  export DISPLAY=$LOCAL_IP:0
-  export LIBGL_ALWAYS_INDIRECT=1
+# locale
+export LC_ALL=en_US.UTF-8 LC_TYPE=en_US.UTF-8 LANG=en_US.UTF-8
 
-  # let vagrant speak to virtualbox
-  export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+# use VcXsrv on WSL to share clipboard
+LOCAL_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
+export DISPLAY=$LOCAL_IP:0
+export LIBGL_ALWAYS_INDIRECT=1
+
+# let vagrant speak to virtualbox
+export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
