561a806df19460d5677e70c6930319246fe44511
Sett Wai <exec@sett.sh>
2020-07-01 19:09:29 +0200

    Only start one dbus and gnome-keyring process each

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,13 @@
 if grep -qEi '(microsoft|wsl)' /proc/version
-	fenv "eval `dbus-launch`" > /dev/null
-	fenv "eval `gnome-keyring-daemon -r -d -c secrets,ssh,pkcs11`" > /dev/null
+	pgrep dbus-daemon > /dev/null
+	if test $status -eq 1
+		fenv "eval `dbus-launch`" > /dev/null
+	end
+
+	pgrep gnome-keyring-d > /dev/null
+	if test $status -eq 1
+		fenv "eval `gnome-keyring-daemon -r -d -c secrets,ssh,pkcs11`" > /dev/null
+	end
 end
 
 source ~/.config/fish/functions/fish_user_aliases.fish
