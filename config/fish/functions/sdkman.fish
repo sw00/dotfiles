f127e5d4e869045d76638ef03378ea7e63b73e34
Sett Wai <exec@sett.sh>
2021-08-19 12:05:40 +0200

    Fix: sdkman paths

--- config/fish/functions/sdkman.fish
+++ config/fish/functions/sdkman.fish
@@ -2,4 +2,4 @@ function sdk
   bash -c '. ~/.sdkman/bin/sdkman-init.sh; sdk "$@"' sdk $argv
 end
 
-set PATH $PATH (find ~/.sdkman/*/current/bin -maxdepth 0)
+set PATH $PATH (find ~/.sdkman/candidates/*/current/bin -maxdepth 0)
