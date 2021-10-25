7e367121d58274331ed98f2807b6a2f41386a5dc
Sett Wai <exec@sett.sh>
2021-10-25 17:19:04 +0200

    Configure paths for Flutter dev

--- config/fish/functions/sdkman.fish
+++ config/fish/functions/sdkman.fish
@@ -3,3 +3,14 @@ function sdk
 end
 
 set PATH $PATH (find ~/.sdkman/candidates/*/current/bin -maxdepth 0)
+
+set -g -x SDKMAN_DIR "$HOME/.sdkman"
+set -g -x JAVA_HOME "$SDKMAN_DIR/candidates/java/current"
+set -g -x PATH $PATH "$JAVA_HOME/bin"
+
+# Android, Flutter Stack:
+set -g -x ANDROID_HOME "$HOME/sdk/Android"
+set -g -x PATH $PATH "$ANDROID_HOME/cmdline-tools/latest"
+set -g -x PATH $PATH "$ANDROID_HOME/cmdline-tools/latest/bin"
+set -g -x PATH $PATH "$ANDROID_HOME/platform-tools"
+
