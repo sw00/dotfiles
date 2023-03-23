500987a790dc2fa8a3e87e05596f73926e8772c9
Sett Wai <exec@sett.sh>
2023-03-23 13:38:25 +0200

    Let git use `gpg`; update gpg settings

--- nixfiles/gpg.nix
+++ nixfiles/gpg.nix
@@ -1,9 +1,16 @@
 { pkgs, config, ... }:
 
 {
+    programs.gpg.enable = true;
+    programs.gpg.settings = {
+        keyid-format = "LONG";
+        with-subkey-fingerprint = true;
+        with-keygrip = true;
+    };
+
     services.gpg-agent = {
         enable = true;
         enableFishIntegration = true;
-        pinentryFlavor = "curses";
+        pinentryFlavor = "qt";
     };
 }
