160b534234e0e21cc448d525e6e5128e9ba08bcc
Sett Wai <exec@sett.sh>
2023-03-30 14:16:13 +0200

    Clear lspconfig error

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -44,7 +44,14 @@ require('mini.trailspace').setup {}
 -- lsp
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'pyright', 'rust_analyzer', 'elixirls', 'ruby_ls' }
+    ensure_installed = {
+        lua_ls = {
+            Lua = {
+                workspace = { checkThirdParty = false },
+                telemetry = { enable = false },
+            },
+        },
+        'pyright', 'rust_analyzer', 'elixirls', 'ruby_ls' }
 })
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
