a37624771641f37b3cd3a8eb023d97148508cc9a
Sett Wai <exec@sett.sh>
2023-04-09 19:24:37 +0200

    Fix: wrapper commands should pass through all args

--- nixfiles/cli.nix
+++ nixfiles/cli.nix
@@ -7,27 +7,23 @@
 
   programs.fish.functions = {
     jq = {
-      argumentNames = "args";
       description = "The json query tool.";
-      body = "nix-shell -p jq --run \"jq $args\"";
+      body = "nix-shell -p jq --run \"jq $argv\"";
     };
 
     yq = {
-      argumentNames = "args";
       description = "The yaml query tool (Golang version).";
-      body = "nix-shell -p jq-go --run \"jq $args\"";
+      body = "nix-shell -p jq-go --run \"jq $argv\"";
     };
 
     howdoi = {
-      argumentNames = "query";
       description = "Ask the internet how to do <query> using the `howdoi` tool.";
-      body = "nix-shell -p python310Packages.howdoi --run \"howdoi $query\"";
+      body = "nix-shell -p python310Packages.howdoi --run \"howdoi $argv\"";
     };
 
     cheat = {
-      argumentNames = "query";
       description = "Print a cheatsheet for a command via the `cheat` tool.";
-      body = "nix-shell -p cheat --run \"cheat $query\"";
+      body = "nix-shell -p cheat --run \"cheat $argv\"";
     };
 
     lrnx = {
@@ -37,15 +33,18 @@
     };
 
     pgcli = {
-      argumentNames = "args";
       description = "Postgres DB command line client with autocompletion.";
-      body = "nix-shell -p pgcli --run \"pgcli $args\"";
+      body = "nix-shell -p pgcli --run \"pgcli $argv\"";
     };
 
     mycli = {
-      argumentNames = "args";
       description = "MySQL command line client with autocompletion.";
-      body = "nix-shell -p mycli --run \"mycli $args\"";
+      body = "nix-shell -p mycli --run \"mycli $argv\"";
+    };
+
+    lazydocker = {
+      description = "TUI for docker.";
+      body = "nix-shell -p lazydocker --run \"lazydocker $argv\"";
     };
   };
 }
