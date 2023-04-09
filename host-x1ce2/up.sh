c78412a4bdd4697d1f8b5b0c2783452b4e429c2c
Sett Wai <exec@sett.sh>
2023-04-09 19:27:53 +0200

    Fix: typo

--- host-x1ce2/up.sh
+++ host-x1ce2/up.sh
@@ -6,7 +6,7 @@ files_list=$(ls -p $FILES_DIR | grep -v /)
 for file in ${files_list[@]}; do
     dest=$(cat $FILES_DIR/$file | head -n 1 | awk '/#/ {print $NF}')
     echo "+++ Copying $FILES_DIR/$file to $dest"
-    if [[ $dest = '/etc/* ]]; then
+    if [[ $dest = /etc/* ]]; then
         sudo cp -f "$FILES_DIR/$file" "$dest"
     else
         cp -f "$FILES_DIR/$file" "$dest"
