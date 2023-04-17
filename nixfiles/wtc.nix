edcd44615c20b7f38c651fefd82d3d3f343d0352
Sett Wai <exec@sett.sh>
2023-04-17 16:40:21 +0200

    Check in LMS env configs

--- nixfiles/wtc.nix
+++ nixfiles/wtc.nix
@@ -6,18 +6,33 @@
     ];
 
     programs.fish.functions = {
-        lms_switch_to = {
-            argumentNames = "lms_env";
-            body = ''
+      lms_switch_to = {
+        argumentNames = "lms_env";
+        body = ''
                 ln -sf $HOME/.config/wtc/$lms_env.yml $HOME/.config/wtc/config.yml
-            '';
-        };
+        '';
+      };
     };
 
     programs.fish.shellAbbrs = {
       wlms = "wtc-lms";
       wadmin = "wtc-admin-cli";
       tf = "terraform";
+    };
+
+    xdg.configFile."prod.yml" = {
+      target = "./wtc/prod.yml";
+      source = ../secrets/wtc/prod.yml;
+    };
+
+    xdg.configFile."tvet.yml" = {
+      target = "./wtc/tvet.yml";
+      source = ../secrets/wtc/tvet.yml;
+    };
+
+    xdg.configFile."bootcamp.yml" = {
+      target = "./wtc/bootcamp.yml";
+      source = ../secrets/wtc/bootcamp.yml;
     };
 }
 
