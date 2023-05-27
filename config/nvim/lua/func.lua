bce102d704a6b9190b7755128b558ef763817abb
Sett Wai <exec@sett.sh>
2023-05-27 17:06:54 +0200

    Redefine scratch function; map keys

--- config/nvim/lua/func.lua
+++ config/nvim/lua/func.lua
@@ -1,7 +1,9 @@
 -- [[scratch buffer]]
-function NewScratch()
+_G.mkScratch = function()
     vim.cmd('tabnew', false)
     vim.opt_local.buftype = 'nofile'
     vim.opt_local.bufhidden = 'hide'
     vim.opt_local.swapfile = nil
 end
+
+vim.api.nvim_set_keymap('n', '<leader>S', '<cmd>lua _G.mkScratch()<cr>', { noremap = true, silent = true})
