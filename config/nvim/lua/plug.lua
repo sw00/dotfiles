7d0bc233f1a100bbfcd2a11f8628d3596efb543c
Sett Wai <exec@sett.sh>
2023-05-27 18:06:28 +0200

    Add toggleterm

--- config/nvim/lua/plug.lua
+++ config/nvim/lua/plug.lua
@@ -33,6 +33,12 @@ return require('packer').startup(function(use)
     -- [[ Editor ]]
     use { 'echasnovski/mini.nvim', branch = 'stable' }
     use 'nathom/filetype.nvim'
+    use {"akinsho/toggleterm.nvim", tag = 'v2.6.0', config = function()
+      require("toggleterm").setup{
+        open_mapping = [[<c-\>]],
+        direction = 'float'
+      }
+    end}
 
     -- [[ Navigation ]]
     use { 'kyazdani42/nvim-tree.lua', tag = 'nightly' }
