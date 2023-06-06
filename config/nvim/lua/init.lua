09ad7334f6a4426a4c70278b4a587c9342355b39
Sett Wai <exec@sett.sh>
2023-06-06 10:47:43 +0200

    Treesitter selection mapping

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -59,6 +59,7 @@ local non_ts_langs = { 'bash', 'yaml', 'json' }
 require('nvim-treesitter.configs').setup {
   ensure_installed = ts_langs,
   sync_install = true, -- install parsers synchronously for ensure_installed langs
+  indent = { enable = true },
 
   -- List of parsers to ignore installing (for "all")
   ignore_install = { "javascript" },
@@ -68,6 +69,16 @@ require('nvim-treesitter.configs').setup {
     disable = non_ts_langs,
     additional_vim_regex_highlighting = non_ts_langs,
   },
+
+  incremental_selection = {
+      enable = true,
+      keymaps = {
+          init_selection = '<leader>s',
+          node_incremental = '<leader>s',
+          scope_incremental = '<c-space>',
+          scope_decremental = '<c-backspace'
+      }
+  }
 }
 
 -- terminal
