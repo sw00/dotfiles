dfaef62586214daa59c6a21177965df658061c24
Sett Wai <exec@sett.sh>
2023-06-06 13:31:14 +0200

    update toggleterm setup

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -57,35 +57,38 @@ local ts_langs = { 'python', 'ruby', 'rust', 'elixir', 'lua' }
 local non_ts_langs = { 'bash', 'yaml', 'json' }
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = ts_langs,
-  sync_install = true, -- install parsers synchronously for ensure_installed langs
-  indent = { enable = true },
+    ensure_installed = ts_langs,
+    sync_install = true, -- install parsers synchronously for ensure_installed langs
+    indent = { enable = true },
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = { "javascript" },
 
-  highlight = {
-    enable = true,
-    disable = non_ts_langs,
-    additional_vim_regex_highlighting = non_ts_langs,
-  },
+    highlight = {
+        enable = true,
+        disable = non_ts_langs,
+        additional_vim_regex_highlighting = non_ts_langs,
+    },
 
-  incremental_selection = {
-      enable = true,
-      keymaps = {
-          init_selection = '<leader>s',
-          node_incremental = '<leader>s',
-          scope_incremental = '<c-space>',
-          scope_decremental = '<c-backspace'
-      }
-  },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<leader>s',
+            node_incremental = '<leader>s',
+            scope_incremental = '<c-space>',
+            scope_decremental = '<c-backspace'
+        }
+    },
 
 }
 
 -- terminal
-require("toggleterm").setup{
+require("toggleterm").setup {
     open_mapping = [[<c-\>]],
-    direction = 'float'
+    direction = 'float',
+    float_opts = {
+        border = 'rounded'
+    }
 }
+vim.api.nvim_set_keymap('n', '<c-~>', '<cmd>ToggleTerm<cr>', { noremap = true, silent = true })
 vim.cmd('au TermOpen * setlocal nonumber norelativenumber')
-
