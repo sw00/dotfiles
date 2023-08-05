58bacaddc37d7cae1b3f3d232e4c4d43e7fb6b25
Sett Wai <exec@sett.sh>
2023-08-05 21:50:13 +0200

    Use nix-generated tmux conf

--- nixfiles/tmux.nix
+++ nixfiles/tmux.nix
@@ -1,7 +1,90 @@
 { config, pkgs, ... }:
-
 {
-  home.file.".tmux.conf".source = ../tmux.conf;
+
+  home.packages = with pkgs; [
+    python310
+    python310Packages.libtmux
+  ];
+
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    # prefix = "C-a";
+    terminal = "tmux-256color";
+    shell = "${pkgs.fish}/bin/fish";
+    newSession = false;
+    historyLimit = 100000;
+    baseIndex = 1;
+    escapeTime = 1;
+    clock24 = true;
+    mouse = true;
+    keyMode = "vi";
+    customPaneNavigationAndResize = true;
+    resizeAmount = 10;
+    secureSocket = false; # for wsl compatibility
+    sensibleOnTop = true;
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = mkTmuxPlugin {
+          pluginName = "tmux-window-rename";
+          rtpFilePath = "tmux_window_name.tmux";
+          version = "19b65ef";
+          src = pkgs.fetchFromGitHub {
+            owner = "ofirgall";
+            repo = "tmux-window-name";
+            rev = "19b65efa8c37501799789194be2a99293e67c632";
+            sha256 = "sha256-VHtnN9XyEv8Gbwq5bJuq8QS04opwDOTGzEcLREy6kBA=";
+          };
+        };
+      }
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+    ];
+
+    extraConfig = ''
+      set -ag terminal-overrides ',*:cud1=\E[1B'
+
+      bind R source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded..."
+
+      bind-key C-A last-window
+
+      bind | split-window -h -c '#{pane_current_path}'
+      bind - split-window -v -c '#{pane_current_path}'
+
+      bind-key -T copy-mode-vi v send -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
+      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
+
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      bind -r C-h resize-pane -L
+      bind -r C-j resize-pane -D
+      bind -r C-k resize-pane -U
+      bind -r C-l resize-pane -R
+
+      set -g status-position top
+      set -g status-justify left
+      set -g status-bg black
+      set -g status-fg white
+
+      set -g window-status-format "#[fg=white] #I:#W "
+      set -g window-status-current-format "#[bg=green,fg=white] #I:#W "
+      set -g window-status-style "bg=black"
+      set -g window-status-last-style "bg=black,fg=green"
+
+      set -g status-right-length 120
+      set -g status-left ""
+
+      set -g status-right "#(wifi_status) | #(ip_addresses) | %b %d %R "
+      set -g status-interval 20
+    '';
+  };
 
   home.file."./bin/ip_addresses" = {
     executable = true;
