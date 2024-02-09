6dd52b4278067dcc759901f7623e85b62ed30fb1
Sett Wai <exec@sett.sh>
2024-02-09 16:01:30 +0200

    Install, configure alacritty for xorg desktop use

--- nixfiles/desktop.nix
+++ nixfiles/desktop.nix
@@ -1,9 +1,46 @@
 # Desktop (non-CLI) apps and config are specified here
-{ config, pkgs, machine_os, ... }: {
-  xdg.enable = if machine_os != "wsl" then true else false;
+{ config, pkgs, fetchFromGitHub, lib, machine_os, ... }:
+let
+  enableOnNonWSL = if machine_os != "wsl " then true else false;
 
-  # Desktop Apps
-  home.packages = with pkgs; [ alacritty ];
+  nerdfonts = pkgs.nerdfonts.override { fonts = [ "CascadiaCode" ]; };
+
+  nixpkgsUnstable = (import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev =
+      "f8e2ebd66d097614d51a56a755450d4ae1632df1"; # nixos-unstable @ 2024-02-09
+    hash = "sha256-2en1kvde3cJVc3ZnTy8QeD2oKcseLFjYPLKhIGDanQ0=";
+  }) { });
+
+  alacrittyPkg = nixpkgsUnstable.alacritty;
+
+  nixgl = import <nixgl> { };
+
+in {
+  xdg.enable = true;
+  fonts.fontconfig.enable = enableOnNonWSL;
+
+  # Desktop Apps, fonts, extras
+  home.packages = [ alacrittyPkg ];
+
+  # Desktop shortcuts
+  xdg.desktopEntries.Alacritty = {
+    name = "Alacritty";
+    genericName = "Terminal";
+    exec =
+      "${nixgl.auto.nixGLDefault}/bin/nixGL ${alacrittyPkg}/bin/alacritty %u";
+    icon = "Alacritty";
+    categories = [ "System" "TerminalEmulator" ];
+    startupNotify = true;
+    actions = {
+      newTerminal = {
+        name = "New Terminal";
+        exec =
+          "${nixgl.auto.nixGLDefault}/bin/nixGL ${alacrittyPkg}/bin/alacritty %u";
+      };
+    };
+  };
 
   # Config files
   home.file = let mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
@@ -24,4 +61,5 @@
       '';
     };
   };
+
 }
