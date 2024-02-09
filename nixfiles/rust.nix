4406dc6da476b570a6d514e16ada03b0ac735dae
Sett Wai <exec@sett.sh>
2024-02-09 12:29:13 +0200

    Initialise rust toolchain in bootstrap

--- nixfiles/rust.nix
+++ nixfiles/rust.nix
@@ -1,1 +1,9 @@
-{ config, pkgs, ... }: { home.packages = with pkgs; [ rustup ]; }
+{ config, pkgs, lib, ... }:
+let defaultToolchain = "stable";
+in with pkgs; {
+  home.packages = [ rustup ];
+  home.activation.initRustToolchain =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      $DRY_RUN_CMD ${rustup}/bin/rustup default ${defaultToolchain}
+    '';
+}
