5e649b9308a3e8f2a24554339d5cfa472cfda487
Sett Wai <exec@sett.sh>
2024-02-12 17:27:56 +0200

    Remove keyboard indicator

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -102,9 +102,6 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
 require('ui')
 
 -- {{{ Mouse bindings
