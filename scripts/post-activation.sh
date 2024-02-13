26c3968c930cf2325f8626ca968df95c97672c2a
Sett Wai <exec@sett.sh>
2024-02-13 04:36:14 +0200

    Amended for awesomewm specific xsession file

--- scripts/post-activation.sh
+++ scripts/post-activation.sh
@@ -2,14 +2,16 @@
 # Tasks to (potentially )complete post-activation of home-manager that cannot be handed by HM.
 # E.g. system-level stuff that requires sudo
 
+set -ex
+
 PREFIX=$HOME/.nix-profile
 
 register_xsessions() {
-    ln -s $PREFIX/share/xsessions/* /usr/share/xsessions/
+    ln -sf ~/.local/share/xsession/awesome.desktop /usr/share/xsessions/
 }
 
 register_udev_rules() {
-    ln -s $PREFIX/etc/udev/rules.d/* /etc/udev/rules.d/
+    ln -sf ~/.nix-profile/etc/udev/rules.d/40-monitor-hotplug.rules /etc/xdg/autostart/
 }
 
 register_xdg_autostarts() {
