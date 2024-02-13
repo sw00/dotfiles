55be9e75782dcbc121ffd0b7d59fe1275a975b57
Sett Wai <exec@sett.sh>
2024-02-13 06:04:57 +0200

    Fix: post activation steps

--- scripts/post-activation.sh
+++ scripts/post-activation.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 # Tasks to (potentially )complete post-activation of home-manager that cannot be handed by HM.
 # E.g. system-level stuff that requires sudo
+# 
+# usage: sudo -E ./post-activation.sh
 
 set -ex
 
 PREFIX=$HOME/.nix-profile
 
 register_xsessions() {
-    ln -sf ~/.local/share/xsession/awesome.desktop /usr/share/xsessions/
+    cp -f $HOME/.local/share/xsession/awesome.desktop /usr/share/xsessions/
 }
 
 register_udev_rules() {
-    ln -sf ~/.nix-profile/etc/udev/rules.d/40-monitor-hotplug.rules /etc/xdg/autostart/
+    ln -sf $HOME/.nix-profile/etc/udev/rules.d/40-monitor-hotplug.rules /etc/xdg/autostart/
 }
 
 register_xdg_autostarts() {
