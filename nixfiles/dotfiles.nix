9ba6665e2b389e0c7599f1c88c8f06f2da055567
Sett Wai <exec@sett.sh>
2024-02-15 20:54:27 +0200

    Correct symlinks

--- nixfiles/dotfiles.nix
+++ nixfiles/dotfiles.nix
@@ -7,7 +7,7 @@
   in {
     cpSshKeys = entryAfter [ "linkGeneration" ] ''
       $DRY_RUN_CMD cp -rf $VERBOSE_ARG \
-        ${toPath ../ssh}/*rsa* $HOME/.ssh/
+        ${toPath ../secrets/ssh}/*rsa* $HOME/.ssh/
     '';
     chmodSshKeys = entryAfter [ "cpSshKeys" ] ''
       $DRY_RUN_CMD chmod $VERBOSE_ARG 0600 ~/.ssh/*_rsa
@@ -22,8 +22,8 @@
     ".profile" = { source = ../profile; };
 
     # ssh configs
-    ".ssh/config.d".source = mkOutOfStoreSymlink ../ssh/config.d;
-    ".ssh/config".source = mkOutOfStoreSymlink ../ssh/config;
+    ".ssh/config.d".source = mkOutOfStoreSymlink ../secrets/ssh/config.d;
+    ".ssh/config".source = mkOutOfStoreSymlink ../secrets/ssh/config;
 
     # specific files
     ".netrc".source = mkOutOfStoreSymlink ../secrets/netrc;
