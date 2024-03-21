2526b31b8036cfdebe750d00e33ca10597de3fa8
Sett Wai <exec@sett.sh>
2024-03-21 18:30:03 +0200

    Add supertab behaviour to autocomplete

--- config/nvim/lua/plugins/autocomplete.lua
+++ config/nvim/lua/plugins/autocomplete.lua
@@ -1,4 +1,4 @@
-return {  -- Autocompletion
+return { -- Autocompletion
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
@@ -40,6 +40,12 @@ return {  -- Autocompletion
         local luasnip = require 'luasnip'
         luasnip.config.setup {}
 
+        local has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
         cmp.setup {
             snippet = {
                 expand = function(args)
@@ -57,6 +63,30 @@ return {  -- Autocompletion
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 -- Select the [p]revious item
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+                -- SuperTab behaviour
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                        -- cmp.confirm({select = true})
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
 
                 -- Scroll the documentation window [b]ack / [f]orward
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
