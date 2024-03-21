c0a43762138809f2707ace7151d4b84ad2960f39
Sett Wai <exec@sett.sh>
2024-03-21 19:31:32 +0200

    Define some extra options

--- config/nvim/lua/opts.lua
+++ config/nvim/lua/opts.lua
@@ -3,60 +3,69 @@ local opt = vim.opt
 local cmd = vim.api.nvim_command
 
 -- [[ Context ]]
-opt.number = true			    -- show line numbers
-opt.relativenumber = true		-- show relative line numbers
-opt.signcolumn = 'number'		-- use number column for signs
-opt.cursorline = true			-- show cursor line
-opt.mouse = 'a'	                -- capture mouse input
+opt.number = true        -- show line numbers
+-- opt.relativenumber = true		-- show relative line numbers
+opt.signcolumn = 'yes'   -- keep signcolumn on
+opt.cursorline = true    -- show cursor line
+opt.inccommand = 'split' -- preview substitutions while typing
+opt.mouse = 'a'          -- capture mouse input
+opt.scrolloff = 10       -- lines above and below cursor
 
 -- [[ Shell ]]
-opt.shell = 'fish'              -- shell
+opt.shell = 'fish' -- shell
 
 -- [[ Code Folds ]]
-opt.foldmethod = "indent"       -- folds defined by indentation
-opt.foldlevelstart = 1          -- only fold if level is higher than 1
+opt.foldmethod = "indent" -- folds defined by indentation
+opt.foldlevelstart = 1    -- only fold if level is higher than 1
 
 -- [[ Filetypes ]]
-opt.encoding = 'utf-8'			-- string encoding
-opt.fileencoding = 'utf-8'		-- file encoding
+opt.encoding = 'utf-8'     -- string encoding
+opt.fileencoding = 'utf-8' -- file encoding
 
 -- [[ Theme ]]
 cmd('colo ' .. OPTIONS.theme)
-opt.syntax = 'on'				-- enable syntax highlighting
-opt.termguicolors = true		-- vim colors override terminal colors
+opt.syntax = 'on'        -- enable syntax highlighting
+opt.termguicolors = true -- vim colors override terminal colors
 
 -- [[ Search ]]
-opt.ignorecase = true			-- ignore case in search terms
-opt.smartcase = true			-- except for capitalised terms
-opt.hlsearch = true			    -- highlight search terms in buffer
-opt.incsearch = true			-- search terms in buffer as they are typed
+opt.ignorecase = true -- ignore case in search terms
+opt.smartcase = true  -- except for capitalised terms
+opt.hlsearch = true   -- highlight search terms in buffer
+opt.incsearch = true  -- search terms in buffer as they are typed
 
 -- [[ Whitespace ]]
-opt.listchars = 'space:·,eol:¬,tab:▸~'	-- show whitespace chars
-opt.expandtab = true			        -- use spaces instead of tabs
-opt.shiftwidth = 4			            -- indent size is 4 chars
-opt.softtabstop = 4			            -- replace tabs with this many spaces in insert mode
-opt.tabstop = 4				            -- number of spaces tabs represent
+opt.list = true -- show whitespace chars
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- show whitespace chars
+opt.expandtab = true -- use spaces instead of tabs
+opt.shiftwidth = 4 -- indent size is 4 chars
+opt.softtabstop = 4 -- replace tabs with this many spaces in insert mode
+opt.tabstop = 4 -- number of spaces tabs represent
+opt.breakindent = true -- wrapped lines should continue from their original indent
 
 -- [[ Splits ]]
-opt.splitbelow = false			-- new hsplit to the top
-opt.splitright = true			-- new vsplit to the right
+opt.splitbelow = false -- new hsplit to the top
+opt.splitright = true  -- new vsplit to the right
 
 -- [[ Completion ]]
-opt.omnifunc = 'syntaxcomplete#Complete' -- vim's default omnifunc
-opt.completeopt = 'menuone,noinsert,noselect'	-- show info for item even if only one
+opt.omnifunc = 'syntaxcomplete#Complete'      -- vim's default omnifunc
+opt.completeopt = 'menuone,noinsert,noselect' -- show info for item even if only one
 
 -- [[ Behaviour ]]
-opt.autowrite = true			-- autosave buffers when losing focus
-opt.hidden = true			-- keep unfocused buffers hidden (instead of unloading them)
-opt.autoread = true			-- reload file if changed outside of vim
-opt.visualbell = true			-- visualbell instead of beeping
+opt.autowrite = true  -- autosave buffers when losing focus
+opt.hidden = true     -- keep unfocused buffers hidden (instead of unloading them)
+opt.autoread = true   -- reload file if changed outside of vim
+opt.visualbell = true -- visualbell instead of beeping
 
 -- [[ Backup/Swapfile ]]
-opt.writebackup = true			-- make backup file before overwriting
-opt.backupdir='~/.vim-tmp,~/.tmp,~/tmp,/var/tmp/,/tmp'
-opt.directory='~/.vim-tmp,~/.tmp,~/tmp,/var/tmp/,/tmp'
+opt.writebackup = true -- make backup file before overwriting
+opt.undofile = true    -- save undo history
+opt.backupdir = '~/.vim-tmp,~/.tmp,~/tmp,/var/tmp/,/tmp'
+opt.directory = '~/.vim-tmp,~/.tmp,~/tmp,/var/tmp/,/tmp'
+opt.undodir = '~/.vim-tmp,~/.tmp,~/tmp,/var/tmp/,/tmp'
 
 -- [[ Clipboard ]]
-opt.clipboard = 'unnamedplus'		-- always use clipboard (instead of vim registers)
+opt.clipboard = 'unnamedplus' -- always use clipboard (instead of vim registers)
 
+-- [[ Optimisations ]]
+opt.updatetime = 250 -- write to swapfile to disk every 250ms
+opt.timeoutlen = 700 -- timeout for a mapped sequence to take
