0a4f075d4f100ae6572fff82162def0148063791
Sett Wai <exec@sett.sh>
2024-03-26 15:00:21 +0200

    Format lua code

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -1,49 +1,53 @@
 return {
-    { 'kyazdani42/nvim-web-devicons', name = 'devicons' },
-    'NLKNguyen/papercolor-theme',
-    {
-        'folke/tokyonight.nvim',
-        priority = 1000,
-        init = function()
-            vim.cmd.colorscheme 'tokyonight-night'
-            vim.cmd.hi 'Comment gui=none'
-        end
-    },
-    'Yggdroot/indentLine',
-    {
-        "folke/zen-mode.nvim",
-        dependencies = 'folke/twilight.nvim',
-        config = function()
-            require("zen-mode").setup {
-                plugins = {
-                    twilight = { enabled = true },
-                    tmux = { enabled = false },
-                    alacritty = { enabled = false, font = "14" }
-                }
-            }
-        end
-    },
-    {
-        'stevearc/dressing.nvim',
-        opts = {
-            input = {
-                enabled = true
-            }
-
-        }
-    },
-    {
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-        },
-    },
-    -- highlight todo, notes in comments
-    { 'folke/todo-comments.nvim',     event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+	{ "kyazdani42/nvim-web-devicons", name = "devicons" },
+	"NLKNguyen/papercolor-theme",
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	"Yggdroot/indentLine",
+	{
+		"folke/zen-mode.nvim",
+		dependencies = "folke/twilight.nvim",
+		config = function()
+			require("zen-mode").setup({
+				plugins = {
+					twilight = { enabled = true },
+					tmux = { enabled = false },
+					alacritty = { enabled = false, font = "14" },
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/dressing.nvim",
+		opts = {
+			input = {
+				enabled = true,
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+	-- highlight todo, notes in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 }
