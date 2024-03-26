63c0ecd6065f47fa7e7c25c18d88f0148d45e86e
Sett Wai <exec@sett.sh>
2024-03-26 14:53:46 +0200

    Replace mini.comment; add format_on_save plugin

--- config/nvim/lua/plugins/ide.lua
+++ config/nvim/lua/plugins/ide.lua
@@ -1,26 +1,25 @@
 return {
-    'tpope/vim-sleuth',
-    { 'tpope/vim-fugitive',      dependencies = { 'tpope/vim-rhubarb', 'shumphrey/fugitive-gitlab.vim' } },
+    { 'tpope/vim-fugitive', dependencies = { 'tpope/vim-rhubarb', 'shumphrey/fugitive-gitlab.vim' } },
     { 'nvim-tree/nvim-tree.lua', opts = {} },
     {
         'j-hui/fidget.nvim',
         opts = {
             integration = {
-                ["nvim-tree"] = { enable = true }
-            }
-        }
+                ['nvim-tree'] = { enable = true },
+            },
+        },
     },
     {
-        "akinsho/toggleterm.nvim",
+        'akinsho/toggleterm.nvim',
         tag = 'v2.6.0',
         opts = {
             open_mapping = [[<c-\>]],
             direction = 'float',
             shell = vim.o.shell,
             float_opts = {
-                border = 'rounded'
-            }
-        }
+                border = 'rounded',
+            },
+        },
     },
     {
         'folke/trouble.nvim',
@@ -28,24 +27,39 @@ return {
         config = function()
             require('trouble').setup {}
 
-            vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-            vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-            vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-            vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-            vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-            vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-        end
+            vim.keymap.set('n', '<leader>xx', function()
+                require('trouble').toggle()
+            end)
+            vim.keymap.set('n', '<leader>xw', function()
+                require('trouble').toggle 'workspace_diagnostics'
+            end)
+            vim.keymap.set('n', '<leader>xd', function()
+                require('trouble').toggle 'document_diagnostics'
+            end)
+            vim.keymap.set('n', '<leader>xq', function()
+                require('trouble').toggle 'quickfix'
+            end)
+            vim.keymap.set('n', '<leader>xl', function()
+                require('trouble').toggle 'loclist'
+            end)
+            vim.keymap.set('n', 'gR', function()
+                require('trouble').toggle 'lsp_references'
+            end)
+        end,
     },
-     { 'vim-test/vim-test', config = function()
-        vim.cmd([[ let test#strategy='toggleterm' ]])
-    end },
-     {
-        "nvim-neotest/neotest",
+    {
+        'vim-test/vim-test',
+        config = function()
+            vim.cmd [[ let test#strategy='toggleterm' ]]
+        end,
+    },
+    {
+        'nvim-neotest/neotest',
         dependencies = {
-            "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
-            "nvim-neotest/neotest-python",
-            "nvim-neotest/neotest-vim-test"
-        }
+            'nvim-lua/plenary.nvim',
+            'antoinemadec/FixCursorHold.nvim',
+            'nvim-neotest/neotest-python',
+            'nvim-neotest/neotest-vim-test',
+        },
     },
 }
--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -3,23 +3,23 @@ return {
         'echasnovski/mini.nvim',
         branch = 'stable',
         config = function()
-            require 'mini.comment'.setup {}
-            require 'mini.pairs'.setup {}
-            require 'mini.trailspace'.setup {}
-            require 'mini.surround'.setup {}
-            require 'mini.bufremove'.setup {}
-            require 'mini.bracketed'.setup {}
+            require('mini.pairs').setup {}
+            require('mini.trailspace').setup {}
+            require('mini.surround').setup {}
+            require('mini.bufremove').setup {}
+            require('mini.bracketed').setup {}
 
             local statusline = require 'mini.statusline'
             statusline.setup { use_icons = true }
             ---@diagnostic disable-next-line: duplicate-set-field
-            statusline.section_location = function() return '%2l:%-2v' end
+            statusline.section_location = function()
+                return '%2l:%-2v'
+            end
 
             require('mini.tabline').setup {
-                tabpage_section = 'none'
+                tabpage_section = 'none',
             }
-            vim.cmd(
-                [[ au FileType * if index(['gitcommit','fugitive'], &ft) >= 0 | let b:minitabline_disable=v:true | endif ]])
-        end
+            vim.cmd [[ au FileType * if index(['gitcommit','fugitive'], &ft) >= 0 | let b:minitabline_disable=v:true | endif ]]
+        end,
     },
 }
