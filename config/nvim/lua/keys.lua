0b036e6933a79c1389f953ff5ab07f9b41b1acf8
Sett Wai <exec@sett.sh>
2024-03-26 15:16:53 +0200

    Clean up shadowed, deprecated keymaps

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -2,74 +2,55 @@
 local map = vim.api.nvim_set_keymap
 
 function map(mode, shortcut, command)
-	vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
 end
 
 local function nmap(shortcut, command)
-	map("n", shortcut, command)
+    map('n', shortcut, command)
 end
 
 local function imap(shortcut, command)
-	map("i", shortcut, command)
+    map('i', shortcut, command)
 end
 
 local function vmap(shortcut, command)
-	map("v", shortcut, command)
+    map('v', shortcut, command)
 end
 
 local function tmap(shortcut, command)
-	map("t", shortcut, command)
+    map('t', shortcut, command)
 end
 
 local vimcmd = function(cmd)
-	return "<cmd>" .. cmd .. "<CR>"
+    return '<cmd>' .. cmd .. '<CR>'
 end
 
 -- [[ QoL ]]
-imap("kj", "<esc>") -- quick escape to normal mode from insert mode
-tmap("kj", "<c-\\><c-n>") -- quick escape to normal mode from terminal mode
-tmap("<esc><esc>", "<c-\\><c-n>") -- quick escape to normal mode from terminal mode
-nmap("<space><space>", ":nohlsearch<CR>") -- cancel search highlights
-nmap("<leader><leader>", "<c-^>") -- switch to previous buffer
-nmap("<leader>ns", "<cmd>lua NewScratch()<cr>") -- open a scratch buffer
+imap('kj', '<esc>') -- quick escape to normal mode from insert mode
+tmap('kj', '<c-\\><c-n>') -- quick escape to normal mode from terminal mode
+tmap('<esc><esc>', '<c-\\><c-n>') -- quick escape to normal mode from terminal mode
+nmap('<space><space>', ':nohlsearch<CR>') -- cancel search highlights
 
 -- [[ Butter Fingers ]]
-nmap(":Q<CR>", ":q<CR>")
-nmap(":Wq<CR>", ":wq<CR>")
-nmap(":WQ<CR>", ":wq!<CR>")
-nmap(":wQ<CR>", ":wq!<CR>")
-nmap(":X<CR>", ":x!<CR>")
+nmap(':Q<CR>', ':q<CR>')
+nmap(':Wq<CR>', ':wq<CR>')
+nmap(':WQ<CR>', ':wq!<CR>')
+nmap(':wQ<CR>', ':wq!<CR>')
+nmap(':X<CR>', ':x!<CR>')
 
 -- [[ Save ]]
-nmap("<F2>", ":w<CR>") -- quicksave
-nmap("<c-s>", ":w<CR>") -- save current buffer
+nmap('<F2>', ':w<CR>') -- quicksave
+nmap('<c-s>', ':w<CR>') -- save current buffer
 
 -- [[ Splits ]]
-nmap("<c-j>", "<c-w>j")
-nmap("<c-k>", "<c-w>k")
-nmap("<c-h>", "<c-w>h")
-nmap("<c-l>", "<c-w>l")
+nmap('<c-j>', '<c-w>j')
+nmap('<c-k>', '<c-w>k')
+nmap('<c-h>', '<c-w>h')
+nmap('<c-l>', '<c-w>l')
 
--- [[ nvim-tree ]]
-nmap("<c-n>", ":NvimTreeToggle<CR>") -- toggle nvim-tree
-nmap("<F3>", ":NvimTreeFindFileToggle<CR>") -- toggle nvim-tree
-
--- [[ version control ]]
-nmap("<c-G>", [[:Git<CR>]])
-
--- [[ vim-test ]]
-nmap("<leader>tt", "<cmd>TestNearest<cr>")
-nmap("<leader>tf", "<cmd>TestFile<cr>")
-nmap("<leader>ta", "<cmd>TestSuite<cr>")
-nmap("<leader>tl", "<cmd>TestLast<cr>")
-nmap("<leader>tg", "<cmd>TestVisit<cr>")
-
--- [[ lspconfig ]]
--- Diagnostics - see `:h vim.diagnostic.*`
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+-- [[ diagnostics ]]
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- [[ zen mode ]]
-nmap("<F12>", vimcmd("ZenMode"))
+nmap('<F12>', vimcmd 'ZenMode')
--- config/nvim/lua/plugins/ide.lua
+++ config/nvim/lua/plugins/ide.lua
@@ -1,65 +1,85 @@
 return {
-	{ "tpope/vim-fugitive", dependencies = { "tpope/vim-rhubarb", "shumphrey/fugitive-gitlab.vim" } },
-	{ "nvim-tree/nvim-tree.lua", opts = {} },
-	{
-		"j-hui/fidget.nvim",
-		opts = {
-			integration = {
-				["nvim-tree"] = { enable = true },
-			},
-		},
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		tag = "v2.6.0",
-		opts = {
-			open_mapping = [[<c-\>]],
-			direction = "float",
-			shell = vim.o.shell,
-			float_opts = {
-				border = "rounded",
-			},
-		},
-	},
-	{
-		"folke/trouble.nvim",
-		dependencies = { "devicons" },
-		config = function()
-			require("trouble").setup({})
+    {
+        'tpope/vim-fugitive',
+        dependencies = { 'tpope/vim-rhubarb', 'shumphrey/fugitive-gitlab.vim' },
+        keys = {
+            { '<c-g>', '<cmd>Git<CR>', desc = 'Fugitive' },
+        },
+    },
+    {
+        'nvim-tree/nvim-tree.lua',
+        opts = {},
+        keys = {
+            { '<c-n>', '<cmd>NvimTreeToggle<cr>', desc = 'NvimTree' },
+            { '<F3>', '<cmd>NvimTreeFindFileToggle<cr>', desc = 'NvimTree FindFile' },
+        },
+    },
+    {
+        'j-hui/fidget.nvim',
+        opts = {
+            integration = {
+                ['nvim-tree'] = { enable = true },
+            },
+        },
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        tag = 'v2.6.0',
+        opts = {
+            open_mapping = [[<c-\>]],
+            direction = 'float',
+            shell = vim.o.shell,
+            float_opts = {
+                border = 'rounded',
+            },
+        },
+    },
+    {
+        'folke/trouble.nvim',
+        dependencies = { 'devicons' },
+        config = function()
+            require('trouble').setup {}
 
-			vim.keymap.set("n", "<leader>xx", function()
-				require("trouble").toggle()
-			end)
-			vim.keymap.set("n", "<leader>xw", function()
-				require("trouble").toggle("workspace_diagnostics")
-			end)
-			vim.keymap.set("n", "<leader>xd", function()
-				require("trouble").toggle("document_diagnostics")
-			end)
-			vim.keymap.set("n", "<leader>xq", function()
-				require("trouble").toggle("quickfix")
-			end)
-			vim.keymap.set("n", "<leader>xl", function()
-				require("trouble").toggle("loclist")
-			end)
-			vim.keymap.set("n", "gR", function()
-				require("trouble").toggle("lsp_references")
-			end)
-		end,
-	},
-	{
-		"vim-test/vim-test",
-		config = function()
-			vim.cmd([[ let test#strategy='toggleterm' ]])
-		end,
-	},
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-neotest/neotest-python",
-			"nvim-neotest/neotest-vim-test",
-		},
-	},
+            vim.keymap.set('n', '<leader>xx', function()
+                require('trouble').toggle()
+            end)
+            vim.keymap.set('n', '<leader>xw', function()
+                require('trouble').toggle 'workspace_diagnostics'
+            end)
+            vim.keymap.set('n', '<leader>xd', function()
+                require('trouble').toggle 'document_diagnostics'
+            end)
+            vim.keymap.set('n', '<leader>xq', function()
+                require('trouble').toggle 'quickfix'
+            end)
+            vim.keymap.set('n', '<leader>xl', function()
+                require('trouble').toggle 'loclist'
+            end)
+            vim.keymap.set('n', 'gR', function()
+                require('trouble').toggle 'lsp_references'
+            end)
+        end,
+    },
+    {
+        'vim-test/vim-test',
+        keys = {
+            { '<leader>tt', '<cmd>TestNearest<cr>', desc = 'vim-test: [T]est [T]his' },
+            { '<leader>tf', '<cmd>TestFile<cr>', desc = '[T]est [F]ile' },
+            { '<leader>ta', '<cmd>TestSuite<cr>', desc = '[T]est [S]uite' },
+            { '<leader>tl', '<cmd>TestLast<cr>', desc = '[T]est [L]ast' },
+            { '<leader>tg', '<cmd>TestVisit<cr>', desc = '[T]est [G]o to last run test' },
+        },
+        config = function()
+            vim.cmd [[ let test#strategy='toggleterm' ]]
+        end,
+    },
+    {
+        'nvim-neotest/neotest',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'antoinemadec/FixCursorHold.nvim',
+            'nvim-neotest/neotest-python',
+            'nvim-neotest/neotest-vim-test',
+        },
+    },
 }
