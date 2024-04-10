496aaf4d709a2f172e14105232959b256e3ff0a8
Sett Wai <exec@sett.sh>
2024-04-10 13:06:27 +0200

    Script to wrap nixos-rebuild

---
+++ nixos-rebuild.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Rebuilds nixos configuration and commits on success.
+
+host=${1:-x1c2e}
+
+set -e
+
+# cd into host config dir
+pushd ~/dotfiles/nixfiles/hosts/$host
+
+$EDITOR configuration.nix
+
+if git diff --quiet '*.nix'; then
+    echo "No changes detected, exiting."
+    popd
+    exit 0
+fi
+
+# show changes
+git diff -U0 '*.nix'
+
+echo "NixOS rebuilding..."
+
+sudo nixos-rebuild switch -I nixos-config=configuration.nix &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+
+# Commit all changes witih the generation metadata
+git commit -am "$current"
+
+popd
+
+# Notify all OK!
+notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
