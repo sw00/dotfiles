33510254a231e6fb202ea727b580b1a886cec667
Sett Wai <exec@sett.sh>
2024-05-18 16:04:21 +0200

    Colorscheme tweaks; make it more like vscode's

--- config/nvim/colors/tomorrow_night_blue.lua
+++ config/nvim/colors/tomorrow_night_blue.lua
@@ -2,19 +2,19 @@
 local palette = {
     base00 = '#002451', -- default bg
     base01 = '#081c3c', -- lighter bg (line numbers, folds, status bars)
-    base02 = '#29293a', -- selection bg
-    base03 = '#3779b7', -- comments, highlights, invisibles
+    base02 = '#11306c', -- selection bg
+    base03 = '#7684b3', -- comments, highlights, invisibles
     base04 = '#f1f1fe', -- dark fg (status bars)
-    base05 = '#aaffff', -- default fg (caret, delimiters, operators)
+    base05 = '#c0d8fb', -- default fg (caret, delimiters, operators)
     base06 = '#ffffff', -- light fg
     base07 = '#1e304e', -- light bg
     base08 = '#f1ffff', -- variables, tags, link text, diff deleted
-    base09 = '#f78d84', -- int, bool, consts, link url
-    base0A = '#fdeea1', -- classes, markup bold
+    base09 = '#f1a2a6', -- int, bool, consts, link url
+    base0A = '#fcefb5', -- classes, markup bold
     base0B = '#d3f1a6', -- strings, inherited classes, diff inserted
     base0C = '#f89c9b', -- support, regex, escape chars, quotes
-    base0D = '#bfdaf6', -- funcs, methods, attribute ids, headings
-    base0E = '#e8bbff', -- keywords, selector, italic, diff changed
+    base0D = '#f6c797', -- funcs, methods, attribute ids, headings
+    base0E = '#e3bdfb', -- keywords, selector, italic, diff changed
     base0F = '#00a5c5', -- deprecated, open/close embedded language tags
 }
 
