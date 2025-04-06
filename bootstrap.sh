6636db18ebfc0e173fa29b75fe6a8b9402cf14d0
Sett Wai <exec@sett.sh>
2025-04-06 16:43:57 +0200

    feat: overhaul dotfiles - migrate to stow

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
+# Bootstrap the dotfiles configurations
 
-sysinfo=$(uname -a)
-if $(echo $sysinfo | grep -qi wsl); then
-    machine_os=wsl
-elif $(echo $sysinfo | grep -qi darwin); then
-    machine_os=macos
+DOTFILES="$HOME/dotfiles"
+
+# Install base configuration for all hosts
+echo "Installing base configurations..."
+stow -d ~/dotfiles/base -t ~ bash git nvim ssh
+
+# Detect and install OS-specific configurations
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Installing macOS configurations..."
+    OS_TYPE=macos
+elif [ "$(uname)" == "Linux" ]; then
+    echo "Installing Linux configurations..."
+    OS_TYPE=linux
+fi
+
+# Find all directories under os-specific folder and stow them
+find "$DOTFILES/os/$OS_TYPE" -maxdepth 1 -type d -not -path "$DOTFILES/os/$OS_TYPE" -exec basename {} \; | xargs -I{} stow -d "$DOTFILES/os/$OS_TYPE" -t ~ {}
+
+# Detect and install host-specific configurations
+HOSTNAME=$(hostname)
+if [ -d "$DOTFILES/hosts/$HOSTNAME" ]; then
+    echo "Installing configurations for host: $HOSTNAME"
+    # Find all directories in the host-specific folder and stow them
+    find "$DOTFILES/hosts/$HOSTNAME" -maxdepth 1 -type d -not -path "$DOTFILES/hosts/$HOSTNAME" -exec basename {} \; | xargs -I{} stow -d "$DOTFILES/hosts/$HOSTNAME" -t ~ {}
 else
-    machine_os=linux
+    echo "No host-specific configurations found for: $HOSTNAME"
 fi
 
-if [[ $1 = --all ]]; then
-    [[ $machine_os = 'wsl' ]] && \
-        pushd ./wsl-up && . up.sh && popd
-elif [[ $1 = --update ]]; then
-	nix-channel --update
-fi
-
-export NIXPKGS_ALLOW_UNFREE=1
-home-manager -f home.nix switch -b backup
-
+echo "Dotfiles installation complete!"
