76ea3d02da03fb96fed78dccac698b8e9573d04c
Sett Wai <exec@sett.sh>
2025-08-07 09:00:52 +0200

    chore: pinentry updated

--- os/macos/gnupg/.gnupg/pinentry-ide.sh
+++ os/macos/gnupg/.gnupg/pinentry-ide.sh
@@ -2,7 +2,7 @@
 if [ -n "$PINENTRY_USER_DATA" ]; then
   case "$PINENTRY_USER_DATA" in
     IJ_PINENTRY=*)
-      "/Applications/PyCharm CE.app/Contents/jbr/Contents/Home/bin/java" -cp "/Applications/PyCharm CE.app/Contents/plugins/vcs-git/lib/git4idea-rt.jar:/Applications/PyCharm CE.app/Contents/lib/externalProcess-rt.jar" git4idea.gpg.PinentryApp
+      "/Applications/PyCharm.app/Contents/jbr/Contents/Home/bin/java" -cp "/Applications/PyCharm.app/Contents/plugins/vcs-git/lib/git4idea-rt.jar:/Applications/PyCharm.app/Contents/lib/externalProcess-rt.jar" git4idea.gpg.PinentryApp
       exit $?
     ;;
   esac
